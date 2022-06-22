64a4611208ee012d9cf62069c3404fc1a1b73066
Rick Ansay <rick.ansay@gmail.com>
2022-06-21 22:21:54 -0600

    Add Fetch functionality

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,7 @@
+import axios, { AxiosResponse } from 'axios';
+
 interface UserProps {
+  id?: number;
   name?: string;
   age?: number;
 }
@@ -8,16 +11,16 @@ type Callback = () => void;
 export class User {
   events: { [key: string]: Callback[] } = {};
 
-  constructor(private data: { name: UserProps}) {}
+  constructor(private data: { name: UserProps }) {}
 
-  get(propName: string): (number | string) { 
+  get(propName: string): number | string {
     return this.data[propName];
   }
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
   }
-  
+
   on(eventName: string, callback: Callback): void {
     const handlers = this.events[eventName] || [];
     handlers.push(callback);
@@ -27,15 +30,20 @@ export class User {
   trigger(eventName: string): void {
     const handlers = this.events[eventName];
 
-    if(!handlers || handlers.length === 0) {
+    if (!handlers || handlers.length === 0) {
       return;
     }
 
-    handlers.forEach(callback => {
+    handlers.forEach((callback) => {
       callback();
     });
   }
 
-  
+  fetch(): void {
+    axios
+      .get(`http://localhost:3000/users/${this.get('id')}`)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data);
+      });
+  }
 }
-
