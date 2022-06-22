c4b6d302723d9a8fc5238d065ab8f09156686c0b
Rick Ansay <rick.ansay@gmail.com>
2022-06-21 19:09:52 -0600

    On event logic

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -130,6 +130,7 @@ var User =
 function () {
   function User(data) {
     this.data = data;
+    this.events = {};
   }
 
   User.prototype.get = function (propName) {
@@ -140,9 +141,11 @@ function () {
     Object.assign(this.data, update);
   };
 
-  User.prototype.on = function (eventName, callback) {};
-
-  User.prototype.trigger = function (eventName, callback) {};
+  User.prototype.on = function (eventName, callback) {
+    var handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  };
 
   return User;
 }();
@@ -161,11 +164,10 @@ var user = new User_1.User({
   name: 'myname',
   age: 20
 });
-user.set({
-  name: 'newname'
-});
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('change', function () {});
+user.on('change', function () {});
+user.on('sdfawsdf', function () {});
+console.log(user);
 },{"./models/User":"src/models/User.ts"}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -194,7 +196,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55433" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54576" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/models/User.ts
+++ src/models/User.ts
@@ -3,9 +3,11 @@ interface UserProps {
   age?: number;
 }
 
-type Callback = () => {}
+type Callback = () => void;
 
 export class User {
+  events: { [key: string]: Callback[] } = {};
+
   constructor(private data: { name: UserProps}) {}
 
   get(propName: string): (number | string) { 
@@ -16,12 +18,14 @@ export class User {
     Object.assign(this.data, update);
   }
   
-  on(eventName: string, callback: Callback) {
-    
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
   }
 
-  trigger(eventName: string, callback: Callback): void {
+  // trigger(eventName: string, callback: Callback): void {
 
-  }
+  // }
 }
 
