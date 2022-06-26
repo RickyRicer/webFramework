242d3bfc5b8ecf2b7d94d26350682c394fe36ec3
Rick Ansay <rick.ansay@gmail.com>
2022-06-25 19:00:28 -0600

    Implement accessors

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,15 @@ import { User } from './models/User';
 
 const user = new User({ name: 'new record', age: 0 });
 
+// A quick reminder on accessors
+
+class Person {
+  constructor(public firstName: string, public lastName: string) {}
+
+  get fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+const person = new Person('firstname', 'lastname');
+console.log(person.fullName);
