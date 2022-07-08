b5f03fe4df43a6adb25ed2f85a678e1805c3a286
Rick Ansay <rick.ansay@gmail.com>
2022-07-08 08:28:07 -0600

    Collection refactor

--- src/models/User.ts
+++ src/models/User.ts
@@ -19,4 +19,11 @@ export class User extends Model<UserProps> {
       new ApiSync<UserProps>(rootUrl)
     );
   }
+
+  static buildUserCollection(): Collection<User, UserProps> {
+    return  new Collection<User, UserProps>(
+      rootUrl,
+      (json: UserProps) => User.buildUser(json)
+    );
+  }
 }
